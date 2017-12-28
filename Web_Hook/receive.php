431117b83f83721831531f83810d2be2f0bfbdb5
Robin Meis <robin@meis.space>
2017-12-28 16:05:09 +0100

    Auth Test

--- Web_Hook/receive.php
+++ Web_Hook/receive.php
@@ -1,5 +1,17 @@
 <?php
 require_once("config.php");
+$headers = getallheaders();
+if (isset($headers["Authorization"])) {
+  $authorization = $headers["Authorization"];
+  $check_auth = $pdo->prepare("SELECT id FROM authorizations WHERE authorization = :authorization LIMIT 1");
+  $check_auth->execute(array($authorization));
+  print_r($pdo->query($check_auth));
+
+} else {
+  print("Error: Authorization Header missing");
+}
+
+exit();
 $pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWD);
 
 $data = array();
