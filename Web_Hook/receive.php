991c36ad5cdb80aec28f5ae2285099e2ceda7f97
Robin Meis <robin@meis.space>
2017-12-28 14:53:18 +0100

    Basic test

---
+++ Web_Hook/receive.php
@@ -0,0 +1,9 @@
+<?php
+require_once("config.php");
+$pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWORD);
+
+$statement = $pdo->prepare("INSERT INTO applications (key) VALUES (?)");
+$statement->execute(json_encode(array(getallheaders())));
+
+$pdo = null;
+?>
