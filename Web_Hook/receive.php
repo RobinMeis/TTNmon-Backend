346eafd50fbaf5d0658cc9f1fc5537fdc119cc68
Robin Meis <robin@meis.space>
2017-12-28 15:07:44 +0100

    Insert test

--- Web_Hook/receive.php
+++ Web_Hook/receive.php
@@ -2,8 +2,11 @@
 require_once("config.php");
 $pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWD);
 
-$statement = $pdo->prepare("INSERT INTO applications (key) VALUES (?)");
-$statement->execute(json_encode(array(getallheaders())));
+$data = array();
+$data['key'] = 'Testfeld';
+
+$statement = $pdo->prepare("INSERT INTO applications (key) VALUES (:key)");
+$statement->execute($data);
 
 $pdo = null;
 ?>
