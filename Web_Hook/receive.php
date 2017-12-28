eb59dccef325b2ec482275a5253f09ecaf90c8de
Robin Meis <robin@meis.space>
2017-12-28 14:54:23 +0100

    Fixed passwd

--- Web_Hook/receive.php
+++ Web_Hook/receive.php
@@ -1,6 +1,6 @@
 <?php
 require_once("config.php");
-$pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWORD);
+$pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWD);
 
 $statement = $pdo->prepare("INSERT INTO applications (key) VALUES (?)");
 $statement->execute(json_encode(array(getallheaders())));
