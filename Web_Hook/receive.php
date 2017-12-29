4767d965e46c897478b51a2fb8f6384ffbc9b271
Robin Meis <robin@meis.space>
2017-12-29 16:38:49 +0100

    Coordinates

--- Web_Hook/receive.php
+++ Web_Hook/receive.php
@@ -20,8 +20,16 @@ if (isset($headers["Authorization"])) {
 
     if (isset($data["metadata"]["modulation"])) {
       if ($data["metadata"]["modulation"] == "LORA") {
-        if (check_array($data, array("hardware_serial", "metadata", "dev_id")) && check_array($data, array("time", "frequency", "data_rate", "bit_rate", "coding_rate", "gateways"))) { //Check packet data for required fields
-          foreach ($data["metadata"]["gateways"] as $gateway) { //Check if all required fields for gateway were transmitted
+        if (check_array($data, array("hardware_serial", "metadata", "dev_id")) && check_array($data, array("time", "frequency", "data_rate", "coding_rate", "gateways"))) { //Check packet data for required fields
+          foreach ($data["metadata"]["gateways"] as $key=>$gateway) { //Check if all required fields for gateway were transmitted
+            if (!isset($gateway["latitude"]) or !isset($gateway["longitude"])) { //gateway lat, lon not available
+              $data["metadata"]["gateways"][$key]["latitude"] = null;
+              $data["metadata"]["gateways"][$key]["longitude"] = null;
+            }
+
+            if (!isset($gateway["altitude"])) //gateway altitude not available
+              $data["metadata"]["gateways"][$key]["altitude"] = null;
+
             if (!check_array($gateway, array("gtw_id", "time", "channel", "rssi", "snr", "rf_chain"))) {
               print("Error: Gateway data incomplete. Required fields are gtw_id, time, channel, rssi, snr, rf_chain");
               exit();
@@ -29,6 +37,16 @@ if (isset($headers["Authorization"])) {
           }
           //Finally we can assume, that we have all required data and continue with data storage
           file_put_contents("log.json", $data); //Log last request
+
+          if (!isset($data["metadata"]["latitude"]) or !isset($data["metadata"]["longitude"])) { //node lat, lon not available
+            $data["metadata"]["latitude"] = null;
+            $data["metadata"]["longitude"] = null;
+          }
+
+          if (!isset($data["metadata"]["altitude"])) //node altitude not available
+            $data["metadata"]["altitude"] = null;
+
+            
 
         } else {
           print("Error: Packet data incomplete. Required fields are hardware_serial, metadata, dev_id, time, frequency, data_rate, bit_rate, coding_rate, gateways");
