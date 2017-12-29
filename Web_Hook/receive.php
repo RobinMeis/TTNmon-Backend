9209c7ac1d8f7be12b323f7c5f1df7b6a0c1a478
Robin Meis <robin@meis.space>
2017-12-29 12:59:50 +0100

    Basic reveive loggin

--- Web_Hook/receive.php
+++ Web_Hook/receive.php
@@ -3,15 +3,36 @@ require_once("config.php");
 $headers = getallheaders();
 if (isset($headers["Authorization"])) {
   $authorization = $headers["Authorization"];
-  $check_auth = $pdo->prepare("SELECT id FROM authorizations WHERE authorization = :authorization LIMIT 1");
+
+  $pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWD);
+
+  $check_auth = $pdo->prepare("SELECT id FROM authorizations WHERE authorization = ? LIMIT 1"); //Check authorization
   $check_auth->execute(array($authorization));
-  print_r($pdo->query($check_auth));
+  $authorization_id = $check_auth->fetch();
+
+  if (!isset($authorization_id["id"])) { //Auth error
+    print("Authorization not found");
+    $pdo = null;
+  } else { //Auth success
+    $authorization_id = $authorization_id["id"];
+    $data = file_get_contents('php://input');
+
+    file_put_contents("log.json", json_encode($_POST));
+
+    $fields_root = array("hardware_serial", "is_retry", "metadata", "gateways"); //Check for required fieds in POST
+    $fields_metadata = array("time", "frequency", "modulation", "data_rate", "bit_rate", "coding_rate", "latitude", "longitude", "altitude");
+    $fields_gateway = array("gtw_id", "time", "channel", "rssi", "snr", "rf_chain", "latitude", "longitude", "altitude");
+    $pdo = null;
+
+  }
 
 } else {
   print("Error: Authorization Header missing");
 }
 
 exit();
+
+
 $pdo = new PDO('mysql:host=localhost;dbname=smrtnoob_ttnmon', $MYSQL_USER, $MYSQL_PASSWD);
 
 $data = array();
@@ -20,5 +41,4 @@ $data['key'] = json_encode(getallheaders());
 $statement = $pdo->prepare("INSERT INTO applications (`key`) VALUES (:key)");
 $statement->execute($data);
 
-$pdo = null;
 ?>
