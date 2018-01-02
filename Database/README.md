2cba0b180683a1160d825a54187d684e14ff57d5
Robin Meis <robin@meis.space>
2018-01-02 21:57:37 +0100

    Added database structure

--- Database/README.md
+++ Database/README.md
@@ -1,1 +1,22 @@
 # Database structure for TTNmon
+The file database_structure.sql contains the database_structure for the backend. It can be restored into a database using phpMyAdmin.
+
+## Table authorizations
+The table authorizations contains the authorization tokens with a maximum length of 20 chars and an auto incremented ID. This token is passed to the Web Hook using the Authorization header.
+
+## Table devices
+The table devices contains the hardware serials of the devices associated to an authorization token.
+- The row authorization_id contains the auto incremented ID from the table authorizations.
+- The row deveui contains the hardware serial provived by TTN. It is possible to register multiple hardware serials for a single authorization token.
+
+## Table packets
+The table packets contains all packets received.
+- CR_k and CR_n means coding rate while it is read like CR_k/CR_n
+- latitude, longitude and altitude are optional and can be setup in the TTN configuration
+- Even if the row modulation is an enum between LORA and FSK, FSK is currently unsupported
+- id is an auto incremented, unique value
+
+## Table gateways
+The table gateways contains the gateways which received a packet
+- packet_id contains the id of the packet
+- There can be multiple gateways which received the same packet, so the packet id might not be unique
---
+++ Database/database_structure.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.15
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 02, 2018 at 09:45 PM
+-- Server version: 5.1.73-log
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `smrtnoob_ttnmon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authorizations`
+--
+
+CREATE TABLE IF NOT EXISTS `authorizations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `authorization` char(20) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `authorization` (`authorization`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices`
+--
+
+CREATE TABLE IF NOT EXISTS `devices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `authorization_id` int(11) NOT NULL,
+  `deveui` binary(8) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `authorization_id` (`authorization_id`),
+  KEY `deveui` (`deveui`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gateways`
+--
+
+CREATE TABLE IF NOT EXISTS `gateways` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `packet_id` int(11) NOT NULL,
+  `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `channel` int(11) NOT NULL,
+  `rssi` int(11) NOT NULL,
+  `snr` int(11) NOT NULL,
+  `rf_chain` int(11) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `altitude` double DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `packet_id` (`packet_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=29 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packets`
+--
+
+CREATE TABLE IF NOT EXISTS `packets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deveui` binary(8) NOT NULL,
+  `time` datetime NOT NULL,
+  `frequency` double NOT NULL,
+  `modulation` enum('LORA','FSK') COLLATE utf8_unicode_ci NOT NULL,
+  `SF` int(11) DEFAULT NULL,
+  `BW` int(11) DEFAULT NULL,
+  `CR_k` int(11) NOT NULL,
+  `CR_n` int(11) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `altitude` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=29 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
