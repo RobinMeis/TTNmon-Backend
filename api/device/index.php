f97f2d445e11165f8f0d26d31cd99dcb9acb0e04
Robin Meis <robin@meis.space>
2018-01-04 00:19:28 +0100

    Register new device

--- api/device/index.php
+++ api/device/index.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") { //get registered devices
     $statement->execute(array($_GET["auth_token"]));
     if (empty($statement->fetch())) { //Auth Error
       $msg["error"] = 1;
-      $msg["msg_en"] = "Invalid authorization code";
+      $msg["msg_en"] = "Invalid authorization token";
     } else { //Auth OK
       $msg["devices"] = array();
       $statement = $pdo->prepare("SELECT deveui FROM devices WHERE authorization = ?");
@@ -26,7 +26,31 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") { //get registered devices
   }
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
   if (isset($_GET["auth_token"]) && isset($_GET["deveui"])) { //All required parameters were specified
+    if (strlen($_GET["deveui"]) != 16 || hex2bin($_GET["deveui"]) === False) { //Invalid deveui length
+      $msg["error"] = 4;
+      $msg["msg_en"] = "Invalid deveui";
+    } else {
+      $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
 
+      $statement = $pdo->prepare("SELECT authorization FROM authorizations WHERE authorization = ?"); //Check authorization
+      $statement->execute(array($_GET["auth_token"]));
+      if (empty($statement->fetch())) { //Auth Error
+        $msg["error"] = 1;
+        $msg["msg_en"] = "Invalid authorization token. The device was not added";
+      } else { //Auth OK
+        $statement = $pdo->prepare("SELECT deveui FROM devices WHERE deveui = ?"); //Check if device is already registered
+        $statement->execute(array(hex2bin($_GET["deveui"])));
+        if (!empty($statement->fetch())) { //Device already registered
+          $msg["error"] = 3;
+          $msg["msg_en"] = "The device is already registered. If you forgot your auth token, please contact me";
+        } else { //Register device
+          $statement = $pdo->prepare("INSERT INTO devices (authorization, deveui, created) VALUES (?, ?, UTC_TIMESTAMP())"); //Register device
+          $statement->execute(array($_GET["auth_token"], hex2bin($_GET["deveui"])));
+          $msg["error"] = 0;
+          $msg["msg_en"] = "The device was added";
+        }
+      }
+    }
   } else { //Parameters missing
     $msg["error"] = 2;
     $msg["msg_en"] = "Invalid request. At least one parameter is missing";
@@ -35,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") { //get registered devices
   if (isset($_GET["auth_token"])) { //All required parameters were specified
 
   } else { //Parameters missing
-    $msg["error"] = 2;
+    $msg["error"] = 3;
     $msg["msg_en"] = "Invalid request. At least one parameter is missing";
   }
 } else {
