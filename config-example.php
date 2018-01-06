2b6dc659ab77e02568e8a98f68cfda96a6ef5292
Robin Meis <robin@meis.space>
2018-01-06 03:42:23 +0100

    Corrected description

--- config-example.php
+++ config-example.php
@@ -5,5 +5,5 @@ $MYSQL_USER                         = "user";
 $MYSQL_PASSWD                       = "password";
 $AUTO_ADOPTION                      = FALSE; //If true, deveui will be added automatically be webhook to authoriziation if device is not registered yet
 $ADOPTION_PROOF                     = FALSE; //If true, a device will be checked with the application key using TTN API before auto adoption
-$ALLOW_MANUAL_DEVICE_REGISTRATION   = TRUE; //Disables device registration using the API. This is recommended when enabling adoption proof as the API can't perform the proof against TTN backend
+$ALLOW_MANUAL_DEVICE_REGISTRATION   = TRUE; //Enables device registration using the API. This is not recommended when enabling adoption proof as the API can't perform the proof against TTN backend
 ?>
