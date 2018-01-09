294b2904533ec4bb95709c6a610eacae5d57ec6f
Robin Meis <robin@meis.space>
2018-01-09 21:54:43 +0100

    Configurable webhook logging

--- config-example.php
+++ config-example.php
@@ -6,4 +6,5 @@ $MYSQL_PASSWD                       = "password";
 $AUTO_ADOPTION                      = FALSE; //If true, deveui will be added automatically be webhook to authoriziation if device is not registered yet
 $ADOPTION_PROOF                     = FALSE; //If true, a device will be checked with the application key using TTN API before auto adoption
 $ALLOW_MANUAL_DEVICE_REGISTRATION   = TRUE; //Enables device registration using the API. This is not recommended when enabling adoption proof as the API can't perform the proof against TTN backend
+$LOG_WEBHOOK                        = FALSE; //Logs the last packet received by the webhook in log.json in /webhook. WARNING: This data is publicly accessible by default. Enable only for debugging
 ?>
