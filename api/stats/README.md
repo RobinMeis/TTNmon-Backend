4ad1fd85f79d68aa6b61a6a6a47dadad1f7f3a3e
Robin Meis <robin@meis.space>
2018-01-14 19:35:39 +0100

    Get table stats

---
+++ api/stats/README.md
@@ -0,0 +1,13 @@
+# /api/stats/
+
+## Method GET
+Returns the current stats
+
+### URL Parameters
+None
+
+### Returns
+Returns current table stats
+
+#### Error code 0
+All good
---
+++ api/stats/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once("../../config.php");
+
+header("Access-Control-Allow-Origin: *");
+
+$msg = array();
+if ($_SERVER['REQUEST_METHOD'] == "GET") { //get registered devices
+  $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
+  $statement = $pdo->prepare("SHOW TABLE STATUS WHERE Name = 'packets' or Name = 'gateways' or Name = 'devices' or Name = 'authorizations'"); //Get table stats
+  $statement->execute();
+  $msg["stats"] = array();
+  while ($table = $statement->fetch()) {
+    $msg["stats"][$table["Name"]] = array();
+    $msg["stats"][$table["Name"]]["count"] = $table["Rows"];
+  }
+  $msg["error"] = 0;
+} else {
+  $msg["error"] = -1;
+  $msg["msg_en"] = "Error: Unsupported method";
+}
+
+print(json_encode($msg));
+?>
