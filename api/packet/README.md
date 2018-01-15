edc45742166bc0a53619ebea6f9da07b1ac5a821
Robin Meis <robin@meis.space>
2018-01-15 16:53:43 +0100

    Get packets

---
+++ api/packet/README.md
@@ -0,0 +1,23 @@
+SPECIFICATION ONLY. NOT YET IMPLEMENTED. MIGHT CHANGE AT ANY TIME
+
+# /api/packet/
+Generates a list of all packets of a specific device. Different filters are available
+
+## Method GET
+
+### Parameters
+#### pseudonym
+Pseudonym of device. NOT DevEUI. If Pseudonym is unknown it can be queried using search
+#### date_start
+Date and time of last packet. Encoded as YYYY-MM-DD_HH-MM-SS (UTC)
+#### date_end
+Date and time of last packet. Encoded as YYYY-MM-DD_HH-MM-SS (UTC)
+
+### Returns
+JSON encoded
+
+#### Error code 0
+Returns list of packets, packet stats and gateways
+
+#### Error code 1
+Required parameters missing
---
+++ api/packet/index.php
@@ -0,0 +1,64 @@
+<?php
+require_once("../../config.php");
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+$msg = array();
+if ($_SERVER['REQUEST_METHOD'] == "GET") { //Get packets
+  if (isset($_GET["date_start"]) && isset($_GET["date_end"]) && isset($_GET["dev_pseudonym"])) {
+    $msg["msg_en"] = "The following packets were received";
+    $msg["error"] = 0;
+    $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
+
+    $statement = $pdo->prepare("SELECT `time`, `packet_count`, `frequency`, `SF`, `BW`, `CR_k`, `CR_n` FROM packets WHERE `dev_pseudonym` = ? and `time` >= ? and `time` <= ?");
+    $statement->execute(array($_GET["dev_pseudonym"], $_GET["date_start"], $_GET["date_end"]));
+
+    $n = 0;
+    $msg["packets"] = array(); //All packets
+    while ($packet = $statement->fetch()) {
+      $msg["packets"][$n] = array();
+      $msg["packets"][$n]["time"] = $packet["time"];
+      $msg["packets"][$n]["packet_count"] = $packet["packet_count"];
+      $msg["packets"][$n]["frequency"] = $packet["frequency"];
+      $msg["packets"][$n]["SF"] = $packet["SF"];
+      $n++;
+    }
+
+    $statement = $pdo->prepare("SELECT count(id) AS packets, MIN(SF) AS SF_min, MAX(SF) AS SF_max FROM packets WHERE `dev_pseudonym` = ? and `time` >= ? and `time` <= ?");
+    $statement->execute(array($_GET["dev_pseudonym"], $_GET["date_start"], $_GET["date_end"]));
+    $packet_stats = $statement->fetch();
+    $msg["packet_stats"] = array(); //Packet stats
+    $msg["packet_stats"]["SF_min"] = $packet_stats["SF_min"];
+    $msg["packet_stats"]["SF_max"] = $packet_stats["SF_max"];
+    $msg["packet_stats"]["packets"] = $packet_stats["packets"];
+
+    $statement = $pdo->prepare("SELECT gtw_id, MIN(snr) AS snr_min, MAX(SNR) as snr_max, MIN(rssi) AS rssi_min, MAX(rssi) AS rssi_max, count(gtw_id) AS packets, gateways.latitude AS lat, gateways.longitude AS lon, gateways.altitude AS alt FROM gateways LEFT JOIN (packets) ON (packets.id = gateways.packet_id) WHERE dev_pseudonym = ? and packets.time >= ? and packets.time <= ? GROUP BY `gtw_id`");
+    $statement->execute(array($_GET["dev_pseudonym"], $_GET["date_start"], $_GET["date_end"]));
+
+    $n = 0;
+    $msg["gateways"] = array();
+    while ($gateway = $statement->fetch()) {
+      $msg["gateways"][$n] = array(); //List of all gateways which received the messages
+      $msg["gateways"][$n]["gtw_id"] = $gateway["gtw_id"];
+      $msg["gateways"][$n]["packets"] = $gateway["packets"];
+      $msg["gateways"][$n]["snr_min"] = $gateway["snr_min"];
+      $msg["gateways"][$n]["snr_max"] = $gateway["snr_max"];
+      $msg["gateways"][$n]["rssi_min"] = $gateway["rssi_min"];
+      $msg["gateways"][$n]["rssi_max"] = $gateway["rssi_max"];
+      $msg["gateways"][$n]["lat"] = $gateway["lat"];
+      $msg["gateways"][$n]["lon"] = $gateway["lon"];
+      $msg["gateways"][$n]["alt"] = $gateway["alt"];
+      $n++;
+    }
+  } else {
+    $msg["error"] = 1;
+    $msg["msg_en"] = "Reuiqred parameters are date_start, date_end and pseudonym";
+  }
+
+} else {
+  $msg["error"] = -1;
+  $msg["msg_en"] = "Error: Unsupported method";
+}
+
+print(json_encode($msg));
+?>
