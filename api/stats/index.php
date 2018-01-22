647206f9c4f5f5853de1d57933164bc19a68ef96
Robin Meis <robin@meis.space>
2018-01-23 00:46:09 +0100

    Added unique gateways

--- api/stats/index.php
+++ api/stats/index.php
@@ -6,12 +6,17 @@ header("Access-Control-Allow-Origin: *");
 $msg = array();
 if ($_SERVER['REQUEST_METHOD'] == "GET") { //get registered devices
   $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
-  $statement = $pdo->prepare("SHOW TABLE STATUS WHERE Name = 'packets' or Name = 'gateways' or Name = 'devices' or Name = 'authorizations'"); //Get table stats
+  $statement = $pdo->prepare("SHOW TABLE STATUS WHERE Name = 'packets' or Name = 'gateways' or Name = 'devices' or Name = 'authorizations' or Name = 'preprocessed_gateway-list'"); //Get table stats
   $statement->execute();
   $msg["stats"] = array();
   while ($table = $statement->fetch()) {
-    $msg["stats"][$table["Name"]] = array();
-    $msg["stats"][$table["Name"]]["count"] = $table["Rows"];
+    if ($table["Name"] == 'preprocessed_gateway-list') {
+      $msg["stats"]["unique_gateways"] = array();
+      $msg["stats"]["unique_gateways"]["count"] = $table["Rows"];
+    } else {
+      $msg["stats"][$table["Name"]] = array();
+      $msg["stats"][$table["Name"]]["count"] = $table["Rows"];
+    }
   }
   $msg["error"] = 0;
 } else {
