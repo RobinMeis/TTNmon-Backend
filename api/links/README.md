a78cef2577f1d53993249e16f84b36b8c4e8aeee
Robin Meis <robin@meis.space>
2018-01-23 19:31:50 +0100

    Get links between gateways and nodes

---
+++ api/links/README.md
@@ -0,0 +1,13 @@
+# /api/links/
+Returns a list of the links between all nodes and gateways with their SNR
+
+## Method GET
+
+### Parameters
+None
+
+### Returns
+JSON encoded
+
+#### Error code 0
+Gateway list
---
+++ api/links/index.php
@@ -0,0 +1,45 @@
+<?php
+require_once("../../config.php");
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+$msg = array();
+if ($_SERVER['REQUEST_METHOD'] == "GET") { //Get packets
+  $msg["msg_en"] = "The following links were detected within the last 7 days";
+  $msg["error"] = 0;
+  $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
+
+  $statement = $pdo->prepare("SELECT `dev_pseudonym`, `gtw_id`, `time`, `snr` FROM `preprocessed_links`");
+  $statement->execute();
+
+    $msg["links"] = array();
+    $n = 0;
+    while ($gateway = $statement->fetch()) {
+      $msg["gateways"][$n] = array();
+      $msg["gateways"][$n]["gtw_id"] = $gateway["gtw_id"];
+      $msg["gateways"][$n]["channels"] = (int)$gateway["channels"];
+      $msg["gateways"][$n]["packets"] = (int)$gateway["packets"];
+      if ($gateway["latitude"] == null || $gateway["longitude"] == null) {
+        $msg["gateways"][$n]["latitude"] = null;
+        $msg["gateways"][$n]["longitude"] = null;
+        $msg["gateways"][$n]["altitude"] = null;
+      } else {
+        $msg["gateways"][$n]["latitude"] = floatval($gateway["latitude"]);
+        $msg["gateways"][$n]["longitude"] = floatval($gateway["longitude"]);
+        if ($gateway["altitude"] == null)
+          $msg["gateways"][$n]["altitude"] = null;
+        else
+          $msg["gateways"][$n]["altitude"] = floatval($gateway["altitude"]);
+      }
+      $msg["gateways"][$n]["first_seen"] = $gateway["first_seen"];
+      $msg["gateways"][$n]["last_seen"] = $gateway["last_seen"];
+      $n++;
+    }
+
+} else {
+  $msg["error"] = -1;
+  $msg["msg_en"] = "Error: Unsupported method";
+}
+
+print(json_encode($msg));
+?>
