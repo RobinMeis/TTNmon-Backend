6330dd4ff7dcf41d9b4674781e9c8653ace40700
Robin Meis <robin@meis.space>
2018-01-23 18:41:34 +0100

    Use latest coordinates
    Now uses latest packet received for getting the coordinates

--- cronjobs/preprocess_gateway-list.php
+++ cronjobs/preprocess_gateway-list.php
@@ -3,6 +3,6 @@ require_once("../config.php");
 $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
 $pdo->query("TRUNCATE `preprocessed_gateway-list`");
 $pdo->query("INSERT INTO `preprocessed_gateway-list` (`gtw_id`, `latitude`, `longitude`, `altitude`, `first_seen`, `last_seen`, `packets`, `channels`)
-  SELECT `gtw_id`, gateways.`latitude`, gateways.`longitude`, gateways.`altitude`, MIN(packets.`time`) AS `first_seen`, MAX(packets.`time`) AS `last_seen`, COUNT(gateways.`id`), COUNT(DISTINCT channel) AS `packets` FROM gateways
-    LEFT JOIN (packets) ON (packets.id = gateways.packet_id) GROUP BY `gtw_id`");
+SELECT gtw_id, latitude, longitude, altitude, MIN(`time`) AS `first_seen`, MAX(`time`) AS `last_seen`, COUNT(`id`) AS `packets`, COUNT(DISTINCT channel) AS `channels` FROM (SELECT `gtw_id`, gateways.`latitude`, gateways.`longitude`, gateways.`altitude`, packets.`time`, gateways.`id`, gateways.`channel` FROM gateways
+  LEFT JOIN (packets) ON (packets.id = gateways.packet_id) ORDER BY packets.`time` DESC) AS base GROUP BY `gtw_id`");
 ?>
