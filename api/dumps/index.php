1fe44a4d17d169304cf6c77b91f549cac664af3a
Robin Meis <robin@meis.space>
2018-01-23 23:34:07 +0100

    Return file information

--- api/dumps/index.php
+++ api/dumps/index.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
+require_once("../../config.php");
 
 $msg = array();
 if ($_SERVER['REQUEST_METHOD'] == "GET") { //Get packets
@@ -8,9 +9,16 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") { //Get packets
   $msg["error"] = 0;
   $msg["dumps"] = array();
   $files = scandir (".");
-  foreach ($files as $file)
-    if (strpos($file, "ttnmon_dump-") !== FALSE)
-      $msg["dumps"][] = $file;
+  foreach ($files as $file) {
+    $n = 0;
+    if (strpos($file, "ttnmon_dump-") !== FALSE) {
+      $msg["dumps"][$n] = array();#
+      $msg["dumps"][$n]["download_url"] = $ENDPOINT_URL ."api/dumps/".$file;
+      $msg["dumps"][$n]["filesize"] = filesize ($file);
+      $msg["dumps"][$n]["filetime"] = date ("c", filemtime ($file));
+      $msg["dumps"][$n]["filename"] = $file;
+    }
+  }
 } else {
   $msg["error"] = -1;
   $msg["msg_en"] = "Error: Unsupported method";
