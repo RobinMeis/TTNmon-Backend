99125f202fa8b26ee90fb1af18cdef32d58124ab
Robin Meis <robin@meis.space>
2018-01-24 00:08:00 +0100

    7 days lifetime

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"
 
 mysqldump smrtnoob_ttnmon packets gateways > ../api/dumps/ttnmon_dump-$(date +%F).sql
-find ../api/dumps/ttnmon_dump-*.sql -mtime +5 -exec rm {} \;
+find ../api/dumps/ttnmon_dump-*.sql -mtime +7 -exec rm {} \;
