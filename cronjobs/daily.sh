dd6d67eb9027034ca132d555e5a2b4d5eaf413d6
Robin Meis <robin@meis.space>
2018-01-25 13:36:58 +0100

    Precalculate SHA-256 checksums

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -3,3 +3,4 @@ cd "$(dirname "$0")"
 
 mysqldump smrtnoob_ttnmon packets gateways > ../api/dumps/ttnmon_dump-$(date +%F).sql
 find ../api/dumps/ttnmon_dump-*.sql -mtime +7 -exec rm {} \;
+sha256sum ../api/dumps/ttnmon_dump-*.sql > ../api/dumps/sha256sums.txt
