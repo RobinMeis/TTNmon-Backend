02b981a00ab0f5dfba2c48cd4101cd34eb101b89
Robin Meis <robin@meis.space>
2018-01-28 23:31:21 +0100

    Log table size daily

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -4,3 +4,4 @@ cd "$(dirname "$0")"
 mysqldump smrtnoob_ttnmon packets gateways > ../api/dumps/ttnmon_dump-$(date +%F).sql
 find ../api/dumps/ttnmon_dump-*.sql -mtime +7 -exec rm {} \;
 sha256sum ../api/dumps/ttnmon_dump-*.sql > ../api/dumps/sha256sums.txt
+php log_table-size.php
