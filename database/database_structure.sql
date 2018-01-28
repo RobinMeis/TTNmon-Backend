8a8c353052dd010969c818fad93910743854e68b
Robin Meis <robin@meis.space>
2018-01-29 00:30:31 +0100

    Database improvements

--- database/database_structure.sql
+++ database/database_structure.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 23, 2018 at 12:14 AM
+-- Generation Time: Jan 29, 2018 at 12:29 AM
 -- Server version: 5.1.73-log
 -- PHP Version: 5.6.32
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `authorizations` (
   `comment` text COLLATE utf8_unicode_ci,
   UNIQUE KEY `authorization` (`authorization`),
   KEY `administrator` (`administrator`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -49,13 +49,13 @@ CREATE TABLE IF NOT EXISTS `devices` (
   `pseudonym` int(11) NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL,
   `last_seen` datetime DEFAULT NULL,
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `altitude` double DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `altitude` decimal(7,2) DEFAULT NULL,
   `comment` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`pseudonym`),
   UNIQUE KEY `deveui` (`deveui`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=29 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=42 ;
 
 -- --------------------------------------------------------
 
@@ -69,16 +69,15 @@ CREATE TABLE IF NOT EXISTS `gateways` (
   `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
   `channel` int(11) NOT NULL,
   `rssi` int(11) NOT NULL,
-  `snr` double NOT NULL,
+  `snr` float NOT NULL,
   `rf_chain` int(11) NOT NULL,
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `altitude` double DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `altitude` decimal(7,2) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `packet_id` (`packet_id`),
-  FULLTEXT KEY `gtw_id` (`gtw_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=191059 ;
+  KEY `packet_id` (`packet_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=507960 ;
 
 -- --------------------------------------------------------
 
@@ -91,20 +90,20 @@ CREATE TABLE IF NOT EXISTS `packets` (
   `dev_pseudonym` int(11) NOT NULL,
   `packet_count` int(11) NOT NULL,
   `time` datetime NOT NULL,
-  `frequency` double NOT NULL,
+  `frequency` float NOT NULL,
   `modulation` enum('LORA','FSK') COLLATE utf8_unicode_ci NOT NULL,
   `SF` int(11) DEFAULT NULL,
   `BW` int(11) DEFAULT NULL,
   `CR_k` int(11) NOT NULL,
   `CR_n` int(11) NOT NULL,
   `gateway_count` int(11) DEFAULT NULL,
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `altitude` double DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `altitude` decimal(7,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `dev_pseudonym` (`dev_pseudonym`),
   KEY `time` (`time`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=88806 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=255183 ;
 
 -- --------------------------------------------------------
 
@@ -117,13 +116,28 @@ CREATE TABLE IF NOT EXISTS `preprocessed_gateway-list` (
   `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
   `channels` int(11) NOT NULL,
   `packets` int(11) NOT NULL,
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `altitude` double DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL,
+  `altitude` decimal(7,2) DEFAULT NULL,
   `first_seen` datetime NOT NULL,
   `last_seen` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=433 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=128 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preprocessed_links`
+--
+
+CREATE TABLE IF NOT EXISTS `preprocessed_links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dev_pseudonym` int(11) NOT NULL,
+  `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `time` datetime NOT NULL,
+  `snr` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=256 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
