741fd81ae36263f8425e70814a877b0a369dd688
Robin Meis <robin@meis.space>
2018-01-28 23:30:44 +0100

    Log table size

---
+++ cronjobs/log_table-size.php
@@ -0,0 +1,31 @@
+<?php
+require_once("../config.php");
+$pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
+
+$sql = "SELECT
+  TABLE_NAME AS `table_name`,
+  ((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024) AS `size_MB`
+FROM
+  information_schema.TABLES
+WHERE TABLE_SCHEMA = '".$MYSQL_DB."'
+ORDER BY
+  (DATA_LENGTH + INDEX_LENGTH)
+DESC";
+
+$csv_data = array(
+  "authorizations" => null,
+  "devices" => null,
+  "packets" => null,
+  "gateways" => null,
+  "preprocessed_gateway-list" => null,
+  "preprocessed_links" => null,
+);
+
+foreach ($pdo->query($sql) as $table)
+  $csv_data[$table["table_name"]] = $table["size_MB"];
+
+if (!file_exists("table-size.csv"))
+  file_put_contents("table-size.csv", "authorizations;devices;packets;gateways;preprocessed_gateway-list;preprocessed_links\n");
+
+file_put_contents("table-size.csv", $csv_data["authorizations"].";".$csv_data["devices"].";".$csv_data["packets"].";".$csv_data["gateways"].";".$csv_data["preprocessed_gateway-list"].";".$csv_data["preprocessed_links"]."\n", FILE_APPEND);
+?>
