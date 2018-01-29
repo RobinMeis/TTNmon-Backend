a0b4dd7c46f87d5b7b0edd8309cd336eb2afd390
Robin Meis <robin@meis.space>
2018-01-29 22:53:41 +0100

    Calculate sum

--- cronjobs/log_table-size.php
+++ cronjobs/log_table-size.php
@@ -21,11 +21,12 @@ $csv_data = array(
   "preprocessed_links" => null,
 );
 
+$sum = 0;
 foreach ($pdo->query($sql) as $table)
   $csv_data[$table["table_name"]] = $table["size_MB"];
-
+  $sum += $table["size_MB"]
 if (!file_exists("table-size.csv"))
-  file_put_contents("table-size.csv", "ISO 8601 date;Size in MB\ndate;authorizations;devices;packets;gateways;preprocessed_gateway-list;preprocessed_links\n");
+  file_put_contents("table-size.csv", "ISO 8601 date;Size in MB\ndate;authorizations;devices;packets;gateways;preprocessed_gateway-list;preprocessed_links;sum\n");
 
-file_put_contents("table-size.csv", date("c").";".$csv_data["authorizations"].";".$csv_data["devices"].";".$csv_data["packets"].";".$csv_data["gateways"].";".$csv_data["preprocessed_gateway-list"].";".$csv_data["preprocessed_links"]."\n", FILE_APPEND);
+file_put_contents("table-size.csv", date("c").";".$csv_data["authorizations"].";".$csv_data["devices"].";".$csv_data["packets"].";".$csv_data["gateways"].";".$csv_data["preprocessed_gateway-list"].";".$csv_data["preprocessed_links"].";".$sum."\n", FILE_APPEND);
 ?>
