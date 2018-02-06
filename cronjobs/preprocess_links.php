cbde47700fe9ccc66a106cec97115ad2a82dda74
Robin Meis <robin@meis.space>
2018-02-06 19:41:33 +0100

    Show only links within last day

--- cronjobs/preprocess_links.php
+++ cronjobs/preprocess_links.php
@@ -5,5 +5,5 @@ $pdo->query("TRUNCATE `preprocessed_links`");
 $pdo->query("INSERT INTO preprocessed_links (gtw_id, dev_pseudonym, time, snr)
 SELECT * FROM (SELECT gtw_id, dev_pseudonym, packets.time, snr FROM gateways LEFT JOIN (packets)
                      ON (packets.id = gateways.packet_id)
-    WHERE packets.time >= CURDATE() - INTERVAL 7 DAY ORDER BY packets.time DESC) AS base GROUP BY dev_pseudonym, gtw_id");
+    WHERE packets.time >= CURDATE() - INTERVAL 1 DAY ORDER BY packets.time DESC) AS base GROUP BY dev_pseudonym, gtw_id");
 ?>
