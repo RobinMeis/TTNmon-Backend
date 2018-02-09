ae73ed8727522fa21ae7f2e650f56c4ad93e2929
Robin Meis <robin@meis.space>
2018-02-09 11:56:02 +0100

    Fix auto removal

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")"
 
 shopt -s nullglob
 mysqldump smrtnoob_ttnmon packets gateways | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
-find ../api/dumps/ttnmon_dump-*.tar.gz -mtime +7 -exec rm {} \;
+find ../api/dumps/ttnmon_dump-*.gz -mtime +7 -exec rm {} \;
 sha256sum ../api/dumps/ttnmon_dump-*.sql.gz > ../api/dumps/sha256sums.txt
 php log_table-size.php
