8cf6fb08276c54007bfce914c6650be1f554450d
Robin Meis <robin@meis.space>
2018-04-11 18:56:18 +0200

    Use new gateway_list

--- api/gateway/list/index.php
+++ api/gateway/list/index.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") { //Get packets
   $msg["error"] = 0;
   $pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
 
-  $statement = $pdo->prepare("SELECT `gtw_id`, `channels`, `packets`, `latitude`, `longitude`, `altitude`, `first_seen`, `last_seen` FROM `preprocessed_gateway-list`");
+  $statement = $pdo->prepare("SELECT `gtw_id`, `channels`, `packets`, `latitude`, `longitude`, `altitude`, `first_seen`, `last_seen` FROM `gateway_list`");
   $statement->execute();
 
     $msg["gateways"] = array();
