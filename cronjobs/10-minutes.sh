d4915feaeddd3fbfbf438de50442586b668d624e
Robin Meis <robin@meis.space>
2018-04-11 20:12:57 +0200

    Not required anymore due to instant storage by webhook

--- cronjobs/10-minutes.sh
+++ cronjobs/10-minutes.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
-php preprocess_gateway-list.php
-php preprocess_links.php
--- cronjobs/preprocess_links.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../config.php");
-$pdo = new PDO('mysql:host='.$MYSQL_SERVER.';dbname='.$MYSQL_DB, $MYSQL_USER, $MYSQL_PASSWD);
-$pdo->query("TRUNCATE `preprocessed_links`");
-$pdo->query("INSERT INTO preprocessed_links (gtw_id, dev_pseudonym, time, snr)
-SELECT * FROM (SELECT gtw_id, dev_pseudonym, packets.time, snr FROM gateways LEFT JOIN (packets)
-                     ON (packets.id = gateways.packet_id)
-    WHERE packets.time >= CURDATE() - INTERVAL 1 DAY ORDER BY packets.time DESC) AS base GROUP BY dev_pseudonym, gtw_id");
-?>
