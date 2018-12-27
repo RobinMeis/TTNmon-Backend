978cd558b31da91f55e175f9155f8d7ebf438b6a
Robin Meis <robin.meis@smartnoob.de>
2018-12-27 12:43:56 +0100

    Support for network prefix and netid resolution

--- database/database_structure.sql
+++ database/database_structure.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb4
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Apr 11, 2018 at 08:55 PM
--- Server version: 10.1.26-MariaDB-0+deb9u1
--- PHP Version: 7.0.27-0+deb9u1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `ttnmon`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `authorizations`
---
 
 CREATE TABLE `authorizations` (
   `authorization` char(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -32,12 +13,6 @@ CREATE TABLE `authorizations` (
   `created` datetime NOT NULL,
   `comment` text COLLATE utf8_unicode_ci
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `devices`
---
 
 CREATE TABLE `devices` (
   `authorization` char(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -53,12 +28,6 @@ CREATE TABLE `devices` (
   `comment` text COLLATE utf8_unicode_ci
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `gateways`
---
-
 CREATE TABLE `gateways` (
   `id` int(11) NOT NULL,
   `packet_id` int(11) NOT NULL,
@@ -71,14 +40,8 @@ CREATE TABLE `gateways` (
   `longitude` decimal(11,8) DEFAULT NULL,
   `altitude` decimal(7,2) DEFAULT NULL,
   `time` datetime DEFAULT NULL,
-  `distance` double DEFAULT NULL
+  `distance` float DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gateway_list`
---
 
 CREATE TABLE `gateway_list` (
   `id` int(11) NOT NULL,
@@ -89,28 +52,53 @@ CREATE TABLE `gateway_list` (
   `longitude` decimal(11,8) DEFAULT NULL,
   `altitude` decimal(7,2) DEFAULT NULL,
   `first_seen` datetime NOT NULL,
-  `last_seen` datetime NOT NULL
+  `last_seen` datetime NOT NULL,
+  `ttn_description` text COLLATE utf8_unicode_ci,
+  `ttn_brand` text COLLATE utf8_unicode_ci,
+  `ttn_model` text COLLATE utf8_unicode_ci,
+  `ttn_username` text COLLATE utf8_unicode_ci,
+  `ttn_updated` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `link_list`
---
+CREATE TABLE `gateway_packets` (
+  `id` int(11) NOT NULL,
+  `type` enum('UPLINK','JOIN') COLLATE utf8_unicode_ci NOT NULL,
+  `frequency` float NOT NULL,
+  `modulation` enum('LORA','FSK') COLLATE utf8_unicode_ci NOT NULL,
+  `SF` int(11) NOT NULL,
+  `BW` int(11) NOT NULL,
+  `CR_k` int(11) NOT NULL,
+  `CR_n` int(11) NOT NULL,
+  `airtime` float NOT NULL,
+  `count` int(11) NOT NULL,
+  `devaddr` binary(4) NOT NULL,
+  `payload_size` int(11) NOT NULL,
+  `RSSI` int(11) NOT NULL,
+  `SNR` int(11) NOT NULL,
+  `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `mapped_device` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `link_list` (
   `id` int(11) NOT NULL,
   `dev_pseudonym` int(11) NOT NULL,
   `gtw_id` text COLLATE utf8_unicode_ci NOT NULL,
   `time` datetime NOT NULL,
-  `snr` double NOT NULL
+  `snr` double NOT NULL,
+  `rssi` int(11) DEFAULT NULL,
+  `gtw_lat` decimal(10,8) DEFAULT NULL,
+  `gtw_lon` decimal(11,8) DEFAULT NULL,
+  `node_lat` decimal(10,8) DEFAULT NULL,
+  `node_lon` decimal(11,8) DEFAULT NULL,
+  `distance` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `packets`
---
+CREATE TABLE `networks` (
+  `prefix` varchar(8) COLLATE utf8_unicode_ci NOT NULL,
+  `location` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `netid` varchar(8) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `packets` (
   `id` int(11) NOT NULL,
@@ -129,80 +117,67 @@ CREATE TABLE `packets` (
   `altitude` decimal(7,2) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for dumped tables
---
+CREATE TABLE `stats_sf` (
+  `id` int(11) NOT NULL,
+  `timestamp_start` datetime NOT NULL,
+  `timestamp_end` datetime NOT NULL,
+  `SF` enum('7','8','9','10','11','12') COLLATE utf8_unicode_ci NOT NULL,
+  `packets` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Indexes for table `authorizations`
---
+
 ALTER TABLE `authorizations`
   ADD UNIQUE KEY `authorization` (`authorization`),
   ADD KEY `administrator` (`administrator`);
 
---
--- Indexes for table `devices`
---
 ALTER TABLE `devices`
   ADD PRIMARY KEY (`pseudonym`),
   ADD UNIQUE KEY `deveui` (`deveui`);
 
---
--- Indexes for table `gateways`
---
 ALTER TABLE `gateways`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `packet_id` (`packet_id`);
+  ADD KEY `packet_id` (`packet_id`),
+  ADD KEY `distance` (`distance`);
 ALTER TABLE `gateways` ADD FULLTEXT KEY `gtw_id` (`gtw_id`);
 
---
--- Indexes for table `gateway_list`
---
 ALTER TABLE `gateway_list`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `link_list`
---
-ALTER TABLE `link_list`
+ALTER TABLE `gateway_packets`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `packets`
---
+ALTER TABLE `link_list`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `link_list` ADD FULLTEXT KEY `gtw_id` (`gtw_id`);
+
+ALTER TABLE `networks`
+  ADD PRIMARY KEY (`prefix`),
+  ADD KEY `netid` (`netid`);
+
 ALTER TABLE `packets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `dev_pseudonym` (`dev_pseudonym`),
   ADD KEY `time` (`time`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `stats_sf`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `timestamp_start` (`timestamp_start`),
+  ADD KEY `timestamp_end` (`timestamp_end`);
 
---
--- AUTO_INCREMENT for table `devices`
---
+
 ALTER TABLE `devices`
   MODIFY `pseudonym` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `gateways`
---
 ALTER TABLE `gateways`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `gateway_list`
---
 ALTER TABLE `gateway_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `link_list`
---
+ALTER TABLE `gateway_packets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `link_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `packets`
---
 ALTER TABLE `packets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `stats_sf`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
