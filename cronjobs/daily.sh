9694f5ec145782fcac2d8a495786bc9994d74ee5
Robin Meis <robin.meis@smartnoob.de>
2018-12-27 13:06:32 +0100

    Dump period changed

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+php gateways_ttn-details.php
 shopt -s nullglob
-mysqldump smrtnoob_ttnmon packets gateways networks | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
-find ../api/dumps/ttnmon_dump-*.gz -mtime +7 -exec rm {} \;
+mysqldump --defaults-file=/home/www-data/.my.cnf ttnmon packets gateways | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
+find ../api/dumps/ttnmon_dump-*.gz -mtime +1 -exec rm {} \;
 sha256sum ../api/dumps/ttnmon_dump-*.sql.gz > ../api/dumps/sha256sums.txt
 php log_table-size.php
