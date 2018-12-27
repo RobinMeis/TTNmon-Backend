c96358f18d73f697fef13e9095158d00d6b5e2cd
Robin Meis <robin.meis@smartnoob.de>
2018-12-27 13:09:09 +0100

    Dump network list

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 php gateways_ttn-details.php
 shopt -s nullglob
-mysqldump --defaults-file=/home/www-data/.my.cnf ttnmon packets gateways | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
+mysqldump --defaults-file=/home/www-data/.my.cnf ttnmon packets gateways networks | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
 find ../api/dumps/ttnmon_dump-*.gz -mtime +1 -exec rm {} \;
 sha256sum ../api/dumps/ttnmon_dump-*.sql.gz > ../api/dumps/sha256sums.txt
 php log_table-size.php
