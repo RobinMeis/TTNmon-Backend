f19692d915111b39b22ce4384c2366489b1c4902
RobinMeis <robin.meis@smartnoob.de>
2019-05-07 20:21:48 +0200

    Fixed truncated lines

--- cronjobs/daily.sh
+++ cronjobs/daily.sh
@@ -3,9 +3,7 @@ cd "$(dirname "$0")"
 
 php gateways_ttn-details.php
 shopt -s nullglob
-mysqldump --defaults-file=/home/www-data/.my.cnf ttnmon packets gateways networks gateway_list | gzip $
-rm /var/www/html/api/dumps/ttnmon_dump-latest.sql.gz
-ln -s  /var/www/html/api/dumps/ttnmon_dump-$(date +%F).sql.gz /var/www/html/api/dumps/ttnmon_dump-late$
+mysqldump --defaults-file=/home/www-data/.my.cnf ttnmon packets gateways networks gateway_list | gzip -c > ../api/dumps/ttnmon_dump-$(date +%F).sql.gz
 find ../api/dumps/ttnmon_dump-*.gz -mtime +1 -exec rm {} \;
 sha256sum ../api/dumps/ttnmon_dump-*.sql.gz > ../api/dumps/sha256sums.txt
 php log_table-size.php
