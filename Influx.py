3ab71c334ddcc476ef3dd967f7313b583ab1584e
RobinMeis <robin.meis@smartnoob.de>
2019-08-07 18:40:50 +0200

    Database Handler

---
+++ Influx.py
@@ -0,0 +1,3 @@
+class Influx:
+    def __init__(self, host, username, password, encryption, ca_cert):
+        print("influx")
---
+++ MySQL.py
@@ -0,0 +1,3 @@
+class MySQL:
+    def __init__(self, host, username, password, encryption, ca_cert):
+        print("mysql")
